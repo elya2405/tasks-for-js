16a01d997daefdc4c2e7d881209b6e2d57834af4
kanybekova eleonora <eelya250@gmail.com>
2023-02-17 13:50:13 +0600

    the first task is to write a function that outputs the first and last name

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1. Напишите функцию, которая выводит ваше имя и фамилию.
+function myName(name) {
+  console.log("My name is " + name);
+}
+myName("Eleonora");
+
+function mySurname(surname) {
+  console.log("My surname is " + surname);
+}
+mySurname("Kanybekova");
