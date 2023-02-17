a8524785c8e40d4c8d8e07cb0450d679800cd08e
kanybekova eleonora <eelya250@gmail.com>
2023-02-17 14:08:15 +0600

    the second task is to write a function that outputs the volume of the cone

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ function mySurname(surname) {
   console.log("My surname is " + surname);
 }
 mySurname("Kanybekova");
+
+// 2. Напишите функцию, которая выводит объем конуса по заданным параметрам - высота (height) и радиус (radius).
+let pi = 3.14;
+function coneVolume(height, radius) {
+  console.log("Объем конуса " + (height * pi * radius ** 2) / 3);
+}
+coneVolume(5, 4);
