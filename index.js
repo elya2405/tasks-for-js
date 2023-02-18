332179fa61114fa4924b291132ee4504609ad4f1
kanybekova eleonora <eelya250@gmail.com>
2023-02-18 23:50:21 +0600

    fixed an error in the first task, wrote the first and last name in one line

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 // 1. Напишите функцию, которая выводит ваше имя и фамилию.
-function myName(name) {
-  console.log("My name is " + name);
+function myNameAndSurname(name, surname) {
+  console.log("My name is " + name + surname);
 }
-myName("Eleonora");
-
-function mySurname(surname) {
-  console.log("My surname is " + surname);
-}
-mySurname("Kanybekova");
+myNameAndSurname("Eleonora ", "Kanybekova");
 
 // 2. Напишите функцию, которая выводит объем конуса по заданным параметрам - высота (height) и радиус (radius).
 let pi = 3.14;
@@ -15,3 +10,9 @@ function coneVolume(height, radius) {
   console.log("Объем конуса " + (height * pi * radius ** 2) / 3);
 }
 coneVolume(5, 4);
+
+// 3. Напишите функцию, которая принимает строку и возвращает ее длину.
+function stringLength(string) {
+  console.log("Длина строки равна " + string.length);
+}
+stringLength("nesten keneshova");
